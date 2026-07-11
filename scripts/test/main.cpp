80b06332e9ce6fb1f6ddfd9712942212372ee73d
prepkg <precompiledpkg@gmail.com>
2026-07-11 14:13:34 +0300

    update test code

--- scripts/test/main.cpp
+++ scripts/test/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <dlib/revision.h>
 
-int main(void) {
+int main() {
     std::cout << DLIB_MAJOR_VERSION << '.' << DLIB_MINOR_VERSION << '.' << DLIB_PATCH_VERSION << std::endl;
 
     return 0;
